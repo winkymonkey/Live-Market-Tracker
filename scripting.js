c152ef8a028594dd72b9326fb036a018ef44cedd
winkymonkey <pal.abhishek.2008@gmail.com>
2021-11-29 23:25:29 +0530

    choose investor option

--- scripting.js
+++ scripting.js
@@ -1,25 +1,44 @@
 $(document).ready(function() {    
     let mapping = {
-        "DEBT_SBI": "https://api.mfapi.in/mf/119824",       //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF200K01VB0&dur=ALL&ind_id=82&classic=true&type=benchmark&investmentType=Debt
-        "SMALL_NIPPON": "https://api.mfapi.in/mf/118778",   //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF204K01K15&dur=ALL&ind_id=9&classic=true&type=benchmark&investmentType=Equity
-        "SMALL_SBI": "https://api.mfapi.in/mf/125497",      //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF200K01T51&dur=ALL&ind_id=26&classic=true&type=benchmark&investmentType=Equity
-        "SMALL_HDFC": "https://api.mfapi.in/mf/130503",     //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF179KA1RW5&dur=ALL&ind_id=53&classic=true&type=benchmark&investmentType=Equity
-        "FLEXI_KOTAK": "https://api.mfapi.in/mf/120166",    //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF174K01LS2&dur=ALL&ind_id=9&classic=true&type=benchmark&investmentType=Equity
-        "FLEXI_PARAG": "https://api.mfapi.in/mf/122639",    //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF879O01027&dur=ALL&ind_id=7&classic=true&type=benchmark&investmentType=Equity
-        "FLEXI_MOTILAL": "https://api.mfapi.in/mf/129046",  //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF247L01502&dur=ALL&ind_id=7&classic=true&type=benchmark&investmentType=Equity
-        "ELSS_NIPPON": "https://api.mfapi.in/mf/103196",    //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF204K01GK4&dur=ALL&ind_id=1&classic=true&type=benchmark&investmentType=Equity
-        "ELSS_MIRAE": "https://api.mfapi.in/mf/135781",     //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF769K01DM9&dur=ALL&ind_id=9&classic=true&type=benchmark&investmentType=Equity
-        "ELSS_MOTILAL": "https://api.mfapi.in/mf/133386",   //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF247L01569&dur=ALL&ind_id=7&classic=true&type=benchmark&investmentType=Equity
-        "LARGE_SBI": "https://api.mfapi.in/mf/103504",      //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF200K01180&dur=ALL&ind_id=1&classic=true&type=benchmark&investmentType=Equity
-        "BALANCED_ICICI": "https://api.mfapi.in/mf/120251"  //https://www.moneycontrol.com/mc/widget/mfnavonetimeinvestment/get_chart_value?isin=INF109K01Y07&dur=ALL&ind_id=9&classic=true&type=benchmark&investmentType=Hybrid
+    	'tumpu': {
+	        'DEBT_SBI': 'https://api.mfapi.in/mf/119824',
+	        'SMALL_NIPPON': 'https://api.mfapi.in/mf/118778',
+	        'SMALL_SBI': 'https://api.mfapi.in/mf/125497',
+	        'SMALL_HDFC': 'https://api.mfapi.in/mf/130503',
+	        'FLEXI_KOTAK': 'https://api.mfapi.in/mf/120166',
+	        'FLEXI_PARAG': 'https://api.mfapi.in/mf/122639',
+	        'FLEXI_MOTILAL': 'https://api.mfapi.in/mf/129046',
+	        'ELSS_NIPPON_REG': 'https://api.mfapi.in/mf/103196',
+	        'ELSS_MIRAE': 'https://api.mfapi.in/mf/135781',
+	        'ELSS_MOTILAL': 'https://api.mfapi.in/mf/133386',
+	        'LARGE_SBI_REG': 'https://api.mfapi.in/mf/103504',
+	        'BALANCED_ICICI': 'https://api.mfapi.in/mf/120251'
+    	},
+    	'ria': {
+    		'DEBT_SBI': 'https://api.mfapi.in/mf/119824',
+    		'SMALL_AXIS': 'https://api.mfapi.in/mf/125354',
+    		'SMALL_SBI': 'https://api.mfapi.in/mf/125497',
+    		'FLEXI_UTI': 'https://api.mfapi.in/mf/120662',
+    		'FLEXI_PARAG': 'https://api.mfapi.in/mf/122639'
+    	},
+    	'maa': {
+    		'DEBT_SBI': 'https://api.mfapi.in/mf/119824',
+    		'SMALL_NIPPON': 'https://api.mfapi.in/mf/118778',
+    		'SMALL_HDFC': 'https://api.mfapi.in/mf/130503',
+    		'FLEXI_KOTAK': 'https://api.mfapi.in/mf/120166',
+    		'FLEXI_PARAG': 'https://api.mfapi.in/mf/122639',
+    		'FLEXI_MOTILAL': 'https://api.mfapi.in/mf/129046',
+    		'LARGE_SBI': 'https://api.mfapi.in/mf/119598',
+    		'BALANCED_ICICI': 'https://api.mfapi.in/mf/120251'
+    	}
     };
     
-    $("#datepicker").datepicker({
+    $('#datepicker').datepicker({
         changeMonth: true,
         changeYear: true,
-        "dateFormat": "dd-mm-yy",
-        "minDate": new Date("January 29, 2013"),
-        "beforeShowDay": $.datepicker.noWeekends
+        'dateFormat': 'dd-mm-yy',
+        'minDate': new Date('January 29, 2013'),
+        'beforeShowDay': $.datepicker.noWeekends
     });
     
     
@@ -27,18 +46,20 @@ $(document).ready(function() {
 		addOverlay();
 	});
     
-    $("#how_many_days").on("focusout", function() {
+    $('#how_many_days').on('focusout', function() {
         clipInput($(this));
     });
     
-    $("#trackerSubmit").click(function() {
-        //generateDailyTracker();
-    	generateMultiDayTracker();
+    $('#trackerSubmit').click(function() {
+    	if (validateMultiDayTracker()) {
+    		generateMultiDayTracker();
+    	}
     });
     
-    $("#dateSubmit").click(function() {
-        //generateOneDayTracker();
-    	generateOneDayTrackerNew();
+    $('#dateSubmit').click(function() {
+    	if (validateOneDayTracker()) {
+    		generateOneDayTracker();
+    	}
     });
     
     
@@ -46,81 +67,123 @@ $(document).ready(function() {
     
     // BUSINESS FUNCTIONS
     function generateMultiDayTracker() {
-    	$('#records_table').empty();
-    	$("#datepicker").val('');
-    	let dayCount = $("#how_many_days").val();
-    	
-    	let promises = [];
-    	let responseArr = [];
-    	
-    	for (key in mapping) {
-    		let fundName = key;
-    		let endpoint = mapping[key];
-    		var settings = {
-                "url": endpoint,
-                "method": "GET",
-                "timeout": 0,
-            };
-    		var request = $.ajax(settings).done(function (response) {
-    			let obj = {
-    				'fundName':fundName, 
-    				'data':response.data
-    			};
-    			responseArr.push(obj);
-    		});
-    		promises.push(request);
+    	let myInvestor = $("input[name='investorName']:checked").val();
+    	try {
+	    	$('#records_table').empty();
+	    	$('#datepicker').val('');
+	    	let dayCount = $('#how_many_days').val();
+	    	
+	    	let promises = [];
+	    	let responseArr = [];
+	    	
+	    	for (key in mapping[myInvestor]) {
+	    		let fundName = key;
+	    		let endpoint = mapping[myInvestor][key];
+	    		var settings = {
+	                'url': endpoint,
+	                'method': 'GET',
+	                'timeout': 0,
+	            };
+	    		var request = $.ajax(settings).done(function (response) {
+	    			let obj = {
+	    				'fundName':fundName, 
+	    				'data':response.data
+	    			};
+	    			responseArr.push(obj);
+	    		})
+	    		.error(function () {
+	    			console.log('error fetching data for fundName='+fundName);
+	    			flashErrorMsg();
+	        		removeOverlay();
+	    		});
+	    		promises.push(request);
+	    	}
+    		
+	    	$.when.apply(null, promises).done(function() {
+	    		responseArr.sort(compare);
+	        	console.log(responseArr);
+	        	processResponse(responseArr, dayCount);
+	        	removeOverlay();
+	        });
     	}
-    	
-    	$.when.apply(null, promises).done(function() {
-    		responseArr.sort(compare);
-        	console.log(responseArr);
-        	proessResponse(responseArr, dayCount);
-        	removeOverlay();
-        });
+    	catch (err) {
+    		console.error('error in generateMultiDayTracker'+err);
+    		flashErrorMsg();
+    		removeOverlay();
+    	}
     }
     
     
-    function generateOneDayTrackerNew() {
-    	$('#records_table').empty();
-        $("#how_many_days").val('');
-        let mydate = $("#datepicker").val();
-        
-        let promises = [];
-    	let responseArr = [];
-    	
-    	for (key in mapping) {
-    		let fundName = key;
-    		let endpoint = mapping[key];
-    		var settings = {
-                "url": endpoint,
-                "method": "GET",
-                "timeout": 0,
-            };
-    		var request = $.ajax(settings).done(function (response) {
-    			let result = response.data.filter(item => item.date==mydate);
-                response.data = result;
-                
-    			let obj = {
-    				'fundName':fundName, 
-    				'data':response.data
-    			};
-    			responseArr.push(obj);
-    		});
-    		promises.push(request);
+    function generateOneDayTracker() {
+    	let myInvestor = $("input[name='investorName']:checked").val();
+    	try {
+	    	$('#records_table').empty();
+	        $('#how_many_days').val('');
+	        let mydate = $('#datepicker').val();
+	        
+	        let promises = [];
+	    	let responseArr = [];
+	    	
+	    	for (key in mapping[myInvestor]) {
+	    		let fundName = key;
+	    		let endpoint = mapping[myInvestor][key];
+	    		var settings = {
+	                'url': endpoint,
+	                'method': 'GET',
+	                'timeout': 0,
+	            };
+	    		var request = $.ajax(settings).done(function (response) {
+	    			let result = response.data.filter(item => item.date==mydate);
+	                response.data = result;
+	                
+	    			let obj = {
+	    				'fundName':fundName, 
+	    				'data':response.data
+	    			};
+	    			responseArr.push(obj);
+	    		})
+	    		.error(function () {
+	    			console.log('error fetching data for fundName='+fundName);
+	    			flashErrorMsg();
+	        		removeOverlay();
+	    		});;
+	    		promises.push(request);
+	    	}
+	    	
+	    	$.when.apply(null, promises).done(function() {
+	    		responseArr.sort(compare);
+	        	console.log(responseArr);
+	        	processResponse(responseArr, 1);
+	        	removeOverlay();
+	        });
     	}
-    	
-    	$.when.apply(null, promises).done(function() {
-    		responseArr.sort(compare);
-        	console.log(responseArr);
-        	proessResponse(responseArr, 1);
-        	removeOverlay();
-        });
+    	catch (err) {
+    		console.error('error in generateOneDayTracker'+err);
+    		flashErrorMsg();
+    		removeOverlay();
+    	}
     }
     
     
     
     
     // Helper function
+    function validateMultiDayTracker() {
+    	if ($('#how_many_days').val() == '') {
+    		flashWarn_invalidInput();
+    		return false;
+    	}
+    	return true;
+    }
+    
+    function validateOneDayTracker() {
+    	if ($('#datepicker').val() == '') {
+    		flashWarn_invalidInput();
+    		return false;
+    	}
+    	return true;
+    }
+    
     function compare(a, b) {
     	if (a.fundName < b.fundName) {
     		return -1;
@@ -131,34 +194,29 @@ $(document).ready(function() {
     	return 0;
     }
     
-    function proessResponse(responseArr, dayCount) {
-    	try {
-	    	let _rowHead = $('<thead>').append($('<th>').text("Fund Name"));
-	    	for (i=0; i<dayCount; i++) {
-	    		if (responseArr[0].data.length > 0) {
-	    			_rowHead.append($('<th>').text(responseArr[0].data[i].date));
-	    		}
-	    		else {
-	    			manageWarningMsg();		//invalid date is chosen
-	    			return;
-	    		}
-	    	}
-	    	$('#records_table').append(_rowHead);
-	    	
-	    	let _rowBody = $('<tbody>');
-	    	for (j=0; j<responseArr.length; j++) {
-	    		let _eachRow = $('<tr>').append($('<td>').text(responseArr[j].fundName));
-	    		for (k=0; k<dayCount; k++) {
-	    			let nav = parseFloat(responseArr[j].data[k].nav).toFixed(4);
-	    			_eachRow.append($('<td>').text(nav));
-	        	}
-	    		_rowBody.append(_eachRow);
-	    	}
-	    	$('#records_table').append(_rowBody);
+    function processResponse(responseArr, dayCount) {
+    	let _rowHead = $('<thead>').append($('<th>').text('Fund Name'));
+    	for (i=0; i<dayCount; i++) {
+    		if (responseArr[0].data.length > 0) {
+    			_rowHead.append($('<th>').text(responseArr[0].data[i].date));
+    		}
+    		else {
+    			flashWarn_noData();		//invalid date is chosen
+    			return;
+    		}
     	}
-    	catch (err) {
-    		flashErrorMsg();
+    	$('#records_table').append(_rowHead);
+    	
+    	let _rowBody = $('<tbody>');
+    	for (j=0; j<responseArr.length; j++) {
+    		let _eachRow = $('<tr>').append($('<td>').text(responseArr[j].fundName));
+    		for (k=0; k<dayCount; k++) {
+    			let nav = parseFloat(responseArr[j].data[k].nav).toFixed(4);
+    			_eachRow.append($('<td>').text(nav));
+        	}
+    		_rowBody.append(_eachRow);
     	}
+    	$('#records_table').append(_rowBody);
     }
     
     
@@ -166,28 +224,36 @@ $(document).ready(function() {
     
     // UTILITY FUNCTIONS
     function addOverlay() {
-        $("#overlay").fadeIn(300);
+        $('#overlay').fadeIn(300);
     }
     
     function removeOverlay() {
         setTimeout(function(){
-            $("#overlay").fadeOut(300);
+            $('#overlay').fadeOut(300);
         },500);
     }
     
     function flashErrorMsg() {
-        $("#5xxError").fadeIn(500);
+        $('#myError').fadeIn(500);
         setTimeout(function(){
-            $("#5xxError").fadeOut(500);
+            $('#myError').fadeOut(500);
         },1500);
     }
     
-    function manageWarningMsg() {
+    function flashWarn_invalidInput() {
+    	$('#records_table').empty();
+        $('#inputWarning').fadeIn(500);
+        setTimeout(function(){
+            $('#inputWarning').fadeOut(500);
+        },1500);
+    }
+
+    function flashWarn_noData() {
         if ($('#records_table tbody tr td:nth-child(2)').length == 0) {
             $('#records_table').empty();
-            $("#DateWarning").fadeIn(500);
+            $('#dateWarning').fadeIn(500);
             setTimeout(function(){
-                $("#DateWarning").fadeOut(500);
+                $('#dateWarning').fadeOut(500);
             },1500);
         }
     }
